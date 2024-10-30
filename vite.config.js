a88afba8ab7fc0a26003ee908565278104040ec4
kianz20 <kianja02@gmail.com>
2024-10-31 01:51:02 +1300

    change back vite config for custom domain

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,6 @@ import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
-export default defineConfig((config) => {
-  return {
-    plugins: [react()],
-    base: config.mode === 'production' ? '/techleaf' : undefined,
-  }
+export default defineConfig({
+  plugins: [react()],
 })
